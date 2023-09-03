2b940f3c26b98c80b9e51732d24f28adb5563479
andrijaristic <andra.rs@outlook.com>
2023-09-03 17:51:46 +0200

    Added migration for property country and area fix

---
+++ Server/Server/Migrations/20230903154203_PropertyLocationAdd.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class PropertyLocationAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Area",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Properties");
+        }
+    }
+}
